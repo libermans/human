cf0cc152796cf27cb480ea28250d8d1a7d2b5258
David and Daniil Liberman <da@liberman.net>
2024-04-18 20:26:25 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlParams.entries());
+  
+    // Initialize Firebase (already done in your HTML file)
+    var database = firebase.database();
+  
+    // Save the data to Firebase
+    database.ref('params/').push(params);
+  });
