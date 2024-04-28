368f9cb6d0a2a528fe40d3b7548812ecb1b38727
David and Daniil Liberman <da@liberman.net>
2024-04-28 14:29:14 +0200

    .

--- a/CHANGELOG.md
+++
@@ -1,1 +0,0 @@
-* Fix password policy error message handling in password reset flow (#1047)
--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const params = Object.fromEntries(urlParams.entries());
   
     // Initialize Firebase (already done in your HTML file)
-    var database = firebase.database();
+    // var database = firebase.database();
   
     // Save the data to Firebase
     database.ref('params/').push(params);
